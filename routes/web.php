f25b0a1cf04af8d638c06a3505314ac61ff71c11
Manuel Miranda <mnlmiranda04@outlook.com>
2022-10-13 19:11:07 -0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', function() {
+    return "Hola";
+});
+
+Route::get('/saludo/{name}', function($name) {
+    return "Hola ".$name;
+});
+
+Route::get('/suma/{num1}/{num2}', function($num1, $num2) {
+    return "suma: ".$num1 + $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::get('/multi/{num1}/{num2}/{num3?}', function($num1, $num2, $num3 = 1) {
+    return "multi: ".$num1 * $num2 * $num3;
+});
